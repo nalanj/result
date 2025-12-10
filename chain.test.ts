2bd47cbfd0733b0a122311a563a88feef153338a
nalanj <5594+nalanj@users.noreply.github.com>
2025-12-09 21:36:28 -0500

    feat: Build out async chains (#1)

--- chain.test.ts
+++ chain.test.ts
@@ -4,11 +4,46 @@ import { chain } from "./chain.ts";
 import { err, ok, type Result } from "./index.ts";
 
 describe("inspect", () => {
-	it("calls the fn and passes on the result", () => {
+	it("calls the fn and passes on the result for ok", () => {
 		let called = false;
 
 		chain(ok("Testing")).inspect((r) => {
 			assert.ok(r.ok);
+			called = true;
+		});
+
+		assert.ok(called);
+	});
+
+	it("calls the fn and passes on the result for err", () => {
+		let called = false;
+
+		chain(err("Testing")).inspect((r) => {
+			assert.ok(!r.ok);
+			called = true;
+		});
+
+		assert.ok(called);
+	});
+});
+
+describe("inspectAsync", () => {
+	it("calls the fn and passes the result for ok", async () => {
+		let called = false;
+
+		chain(ok("Testing")).inspectAsync(async (r) => {
+			assert.ok(r.ok);
+			called = true;
+		});
+
+		assert.ok(called);
+	});
+
+	it("calls the fn and passes the result for err", async () => {
+		let called = false;
+
+		chain(err("Testing")).inspectAsync(async (r) => {
+			assert.ok(!r.ok);
 			called = true;
 		});
 
@@ -40,6 +75,30 @@ describe("map", () => {
 	});
 });
 
+describe("mapAsync", () => {
+	it("handles OK", async () => {
+		const res = ok("Alan") as Result<string, string>;
+
+		const mapped = await chain(res)
+			.mapAsync(async (name: string) => `Hello ${name}`)
+			.result();
+
+		assert.ok(mapped.ok);
+		assert.equal(mapped.value, "Hello Alan");
+	});
+
+	it("ignores Err", async () => {
+		const res = err("It broke") as Result<string, string>;
+
+		const mapped = await chain(res)
+			.mapAsync(async (name: string) => `Hello ${name}`)
+			.result();
+
+		assert.ok(!mapped.ok);
+		assert.equal(mapped.err, "It broke");
+	});
+});
+
 describe("mapErr", () => {
 	it("ignores OK", () => {
 		const res = ok("Alan") as Result<string, string>;
@@ -57,6 +116,30 @@ describe("mapErr", () => {
 
 		const mapped = chain(res)
 			.mapErr((cause: string) => `It broke because ${cause}`)
+			.result();
+
+		assert.ok(!mapped.ok);
+		assert.equal(mapped.err, "It broke because a crash");
+	});
+});
+
+describe("mapErrAsync", () => {
+	it("ignores OK", async () => {
+		const res = ok("Alan") as Result<string, string>;
+
+		const mapped = await chain(res)
+			.mapErrAsync(async (cause: string) => `It broke because ${cause}`)
+			.result();
+
+		assert.ok(mapped.ok);
+		assert.equal(mapped.value, "Alan");
+	});
+
+	it("handles Err", async () => {
+		const res = err("a crash");
+
+		const mapped = await chain(res)
+			.mapErrAsync(async (cause: string) => `It broke because ${cause}`)
 			.result();
 
 		assert.ok(!mapped.ok);
@@ -88,6 +171,30 @@ describe("andThen", () => {
 	});
 });
 
+describe("andThenAsync", () => {
+	it("handles OK", async () => {
+		const res = ok("Alan") as Result<string, string>;
+
+		const andThenned = await chain(res)
+			.andThenAsync(async (r) => ok(`Hello ${r.value}`))
+			.result();
+
+		assert.ok(andThenned.ok);
+		assert.equal(andThenned.value, "Hello Alan");
+	});
+
+	it("ignores Err", async () => {
+		const res = err("Ugh") as Result<string, string>;
+
+		const andThenned = await chain(res)
+			.andThenAsync(async (r) => ok(`Hello ${r.value}`))
+			.result();
+
+		assert.ok(!andThenned.ok);
+		assert.equal(andThenned.err, "Ugh");
+	});
+});
+
 describe("orElse", () => {
 	it("ignores OK", () => {
 		const res = ok("Alan") as Result<string, string>;
@@ -112,6 +219,30 @@ describe("orElse", () => {
 	});
 });
 
+describe("orElseAsync", () => {
+	it("ignores OK", async () => {
+		const res = ok("Alan") as Result<string, string>;
+
+		const orElsed = await chain(res)
+			.orElseAsync(async (r) => err(`Or elsed: ${r.err}`))
+			.result();
+
+		assert.ok(orElsed.ok);
+		assert.equal(orElsed.value, "Alan");
+	});
+
+	it("handles Err", async () => {
+		const res = err("Ugh") as Result<string, string>;
+
+		const orElsed = await chain(res)
+			.orElseAsync(async (r) => err(`Or elsed: ${r.err}`))
+			.result();
+
+		assert.ok(!orElsed.ok);
+		assert.equal(orElsed.err, "Or elsed: Ugh");
+	});
+});
+
 describe("unwrap", () => {
 	it("returns value for OK", () => {
 		const res = ok("Alan") as Result<string, string>;
@@ -119,9 +250,31 @@ describe("unwrap", () => {
 		assert.equal(chain(res).unwrap(), "Alan");
 	});
 
+	it("returns value for OK async", async () => {
+		const res = ok("Alan") as Result<string, string>;
+
+		assert.equal(
+			await chain(res)
+				.mapAsync(async (name) => name)
+				.unwrap(),
+			"Alan",
+		);
+	});
+
 	it("throws for Err", () => {
 		const res = err("Ugh") as Result<string, string>;
 
 		assert.throws(() => chain(res).unwrap());
+	});
+
+	it("throws for Err async", async () => {
+		const res = err("Ugh") as Result<string, string>;
+
+		await assert.rejects(
+			async () =>
+				await chain(res)
+					.mapAsync(async (name) => name)
+					.unwrap(),
+		);
 	});
 });
--- chain.ts
+++ chain.ts
@@ -1,14 +1,41 @@
-import { type Err, err, type OK, ok, type Result } from "./index.ts";
+import { type Err, err, type OK, ok, type Result, unwrap } from "./index.ts";
 
 export type ResultChain<T, E> = {
 	inspect(fn: (r: Result<T, E>) => void): ResultChain<T, E>;
+	inspectAsync(fn: (r: Result<T, E>) => Promise<void>): AsyncResultChain<T, E>;
 	map<U>(fn: (t: T) => U): ResultChain<U, E>;
+	mapAsync<U>(fn: (t: T) => Promise<U>): AsyncResultChain<U, E>;
 	mapErr<S>(fn: (e: E) => S): ResultChain<T, S>;
+	mapErrAsync<S>(fn: (e: E) => Promise<S>): AsyncResultChain<T, S>;
 	andThen<U>(fn: (r: OK<T>) => Result<U, E>): ResultChain<U, E>;
+	andThenAsync<U>(
+		fn: (r: OK<T>) => Promise<Result<U, E>>,
+	): AsyncResultChain<U, E>;
 	orElse<S>(fn: (r: Err<E>) => Result<T, S>): ResultChain<T, S>;
+	orElseAsync<S>(
+		fn: (r: Err<E>) => Promise<Result<T, S>>,
+	): AsyncResultChain<T, S>;
 
 	result(): Result<T, E>;
 	unwrap(): T;
+};
+
+export type AsyncResultChain<T, E> = {
+	inspect(fn: (r: Result<T, E>) => void): AsyncResultChain<T, E>;
+	inspectAsync(fn: (r: Result<T, E>) => Promise<void>): AsyncResultChain<T, E>;
+	map<U>(fn: (t: T) => U): AsyncResultChain<U, E>;
+	mapAsync<U>(fn: (t: T) => Promise<U>): AsyncResultChain<U, E>;
+	andThen<U>(fn: (r: OK<T>) => Result<U, E>): AsyncResultChain<U, E>;
+	andThenAsync<U>(
+		fn: (r: OK<T>) => Promise<Result<U, E>>,
+	): AsyncResultChain<U, E>;
+	orElse<S>(fn: (r: Err<E>) => Result<T, S>): AsyncResultChain<T, S>;
+	orElseAsync<S>(
+		fn: (r: Err<E>) => Promise<Result<T, S>>,
+	): AsyncResultChain<T, S>;
+
+	result(): Promise<Result<T, E>>;
+	unwrap(): Promise<T>;
 };
 
 export function chain<T, E>(r: Result<T, E>): ResultChain<T, E> {
@@ -19,42 +46,84 @@ export function chain<T, E>(r: Result<T, E>): ResultChain<T, E> {
 	return errChain(r);
 }
 
+export function asyncChain<T, E>(
+	promise: Promise<Result<T, E>>,
+): AsyncResultChain<T, E> {
+	return {
+		inspect: (fn) =>
+			asyncChain(promise.then((r) => chain(r).inspect(fn).result())),
+		inspectAsync: (fn) =>
+			asyncChain(promise.then((r) => chain(r).inspectAsync(fn).result())),
+		map: (fn) => asyncChain(promise.then((r) => chain(r).map(fn).result())),
+		mapAsync: (fn) =>
+			asyncChain(promise.then((r) => chain(r).mapAsync(fn).result())),
+		andThen: (fn) =>
+			asyncChain(promise.then((r) => chain(r).andThen(fn).result())),
+		andThenAsync: (fn) =>
+			asyncChain(promise.then((r) => chain(r).andThenAsync(fn).result())),
+		orElse: (fn) =>
+			asyncChain(promise.then((r) => chain(r).orElse(fn).result())),
+		orElseAsync: (fn) =>
+			asyncChain(promise.then((r) => chain(r).orElseAsync(fn).result())),
+
+		result: () => {
+			return promise;
+		},
+		unwrap: async () => {
+			const r = await promise;
+			return unwrap(r);
+		},
+	};
+}
+
 function okChain<T, E>(r: OK<T>): ResultChain<T, E> {
-	const c: ResultChain<T, E> = {
-		...r,
+	return {
 		inspect: (fn) => {
 			fn(r);
 
-			return c;
+			return okChain(r);
+		},
+		inspectAsync: (fn) => {
+			const promise = fn(r).then(() => r);
+
+			return asyncChain(promise);
 		},
 		map: (fn) => chain(ok(fn(r.value))),
+		mapAsync: (fn) => asyncChain(fn(r.value).then((v) => ok(v))),
 		mapErr: (_) => chain(r),
+		mapErrAsync: (_) => asyncChain(Promise.resolve(r)),
 		andThen: (fn) => chain(fn(r)),
+		andThenAsync: (fn) => asyncChain(fn(r)),
 		orElse: (_) => chain(r),
+		orElseAsync: (_) => asyncChain(Promise.resolve(r)),
 		result: () => r,
 		unwrap: () => r.value,
 	};
-
-	return c;
 }
 
 function errChain<T, E>(r: Err<E>): ResultChain<T, E> {
-	const c: ResultChain<T, E> = {
-		...r,
+	return {
 		inspect: (fn) => {
 			fn(r);
 
-			return c;
+			return errChain(r);
+		},
+		inspectAsync: (fn) => {
+			const promise = fn(r).then(() => r);
+
+			return asyncChain(promise);
 		},
 		map: (_) => chain(r),
+		mapAsync: (_) => asyncChain(Promise.resolve(r)),
 		mapErr: (fn) => chain(err(fn(r.err))),
+		mapErrAsync: (fn) => asyncChain(fn(r.err).then((e) => err(e))),
 		andThen: (_) => chain(r),
+		andThenAsync: (_) => asyncChain(Promise.resolve(r)),
 		orElse: (fn) => chain(fn(r)),
+		orElseAsync: (fn) => asyncChain(fn(r)),
 		result: () => r,
 		unwrap: () => {
 			throw r.err;
 		},
 	};
-
-	return c;
 }
