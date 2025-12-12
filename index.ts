e56605bb6a1af4f6b34d98a7b8e2099ac554549c
Alan Johnson <nalanj@fastmail.com>
2025-12-12 08:47:50 -0500

    Start on typedocs

--- index.ts
+++ index.ts
@@ -12,14 +12,25 @@ export type Err<E> = {
 
 export type Result<T, E> = OK<T> | Err<E>;
 
+/**
+ * Creates an `OK` value representing a successful result.
+ */
 export function ok<T>(t: T): OK<T> {
 	return { ok: true, value: t };
 }
 
+/**
+ * Creates an `Err` value respresenting a failure.
+ *
+ * While you will very often have an `Err<Error>` as a type to wrap exceptions, it's not required.
+ */
 export function err<E>(e: E): Err<E> {
 	return { ok: false, err: e };
 }
 
+/**
+ * Returns a value of type `T` or throws an error.
+ */
 export function unwrap<T, E>(r: Result<T, E>): T {
 	if (r.ok) {
 		return r.value;
